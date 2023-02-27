86feea53629086b782f4166b133940549cb57b66
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2023-02-27 15:30:36 +0300

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary
+ * @tree: node to check
+ * Return: the count
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) +
+				binary_tree_leaves(tree->right));
+}
